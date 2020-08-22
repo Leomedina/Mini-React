02ea8edb09c8d97e5b200861ab125c3a915ac00a
Leo <medinayleo@gmail.com>
2020-08-22 18:31:32 -0400

    render element function

--- ReactElement.js
+++ ReactElement.js
@@ -5,32 +5,51 @@
  * 
 */
 
-class MiniReact {
 
-  static createElement(type, props, ...children) {
-    return {
-      type,
-      props: {
-        ...props,
-        children: children.map(child =>
-          typeof child === "object"
-            ? child
-            : this.createTextElement(child))
-      }
-    };
+//Create react element and if the children is not an object it will make a default text element
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : this.createTextElement(child))
+    }
   };
+};
 
-  static createTextElement(text) {
-    return {
-      type: "TEXT_ELEMENT",
-      props: {
-        nodeValue: text,
-        children: []
-      }
-    };
+//basic text element
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
   };
+};
 
+function render(element, container) {
+  const domElement =
+    element.type = "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = key => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => { domElement[name] = element.props[name] })
+
+  element.props.children.forEach(child => render(child, domElement))
+
+  container.appendChild(domElement);
 };
 
 
-module.exports = MiniReact;
+const MiniReact = {
+  createElement,
+  render
+};
+
